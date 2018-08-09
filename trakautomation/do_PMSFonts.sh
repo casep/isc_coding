41af90466a8e3557aa45d3b3fbcfdbce5a0b89bf
Carlos "casep" Sepulveda <casep@fedoraproject.org>
2018-08-09 16:52:47 +0100

    Updated font deployment script to deploy new Free 3 of 9 font

--- trakautomation/do_PMSFonts.sh
+++ trakautomation/do_PMSFonts.sh
@@ -1,12 +1,13 @@
 #!/bin/sh -e
 # Install extra fonts used for reports (Print & Preview)
 # Glen Pitt-Pladdy (ISC)
+# Casep, 20180809, adding Free to the deployment
 . ./functions.sh
 
 
 check_LINUX() {
         BASEPATH=/usr/share/fonts
-        if [ -d $BASEPATH/PMSFonts/ ]; then return 0; fi
+        if [ -d $BASEPATH/PMSFonts ] || [ -d $BASEPATH/Free3of9 ] ; then return 0; fi
         return 1
 }
 
@@ -15,6 +16,7 @@ install_LINUX() {
 		mkdir -p $BASEPATH
 	fi
 	tar xzf archives/PMSFonts.tar.gz -C $BASEPATH/
+	tar xzf archives/free3of9.tar.gz -C $BASEPATH/
 	/usr/bin/fc-cache -f $BASEPATH
 }
 
